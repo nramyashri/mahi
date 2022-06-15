54d6860094f7a60b9ad60f77ce2bae2934ec5a0a
nramyashri <101247596+nramyashri@users.noreply.github.com>
2022-06-15 14:29:53 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,8 @@
+function mystud(){
+    console.log("cvr college of engineering");
+}
+mystud();
+
+console.log("welcome to cvr");
+console.log("welcome to itA");
+console.log("this is it department");
