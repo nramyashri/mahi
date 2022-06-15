57eebfbf5bf4f02ed4a17853a78719ad5d500b98
nramyashri <101247596+nramyashri@users.noreply.github.com>
2022-06-15 14:30:28 +0530

    Delete sample.js

--- sample.js
+++
@@ -1,8 +0,0 @@
-function mystud(){
-    console.log("cvr college of engineering");
-}
-mystud();
-
-console.log("welcome to cvr");
-console.log("welcome to itA");
-console.log("this is it department");
